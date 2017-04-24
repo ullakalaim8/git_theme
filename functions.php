ec81d3da7f7233693065d7e89512fca4f9ba6480
ullakalaim8 <kaleemon.mohammed@gmail.com>
2017-04-24 16:54:28 +0530

    tet
    test

--- functions.php
+++ functions.php
@@ -1,3 +1,14 @@
 <?php
 
 
+if( ! class_exists( 'themeupdater' ) ){
+	include_once( plugin_dir_path( __FILE__ ) . 'theme-updater.php' );
+}
+
+$updater = new themeupdater('git_theme');
+$updater->set_username( 'ullakalaim8' );
+$updater->set_repository( 'git_theme' );
+/*
+	$updater->authorize( 'abcdefghijk1234567890' ); // Your auth code goes here for private repos
+*/
+$updater->initialize();
